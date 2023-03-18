65c14ce4a6df3b930703301fd69f32ec2a4fa0d2
tillybeamen <till.pugh@gmail.com>
2023-03-18 11:48:05 -0700

    loop challenges

---
+++ factorial.js
@@ -0,0 +1,7 @@
+var product = 1
+
+for(var i = 1; i < 13; i ++) {
+    product = product * i
+}
+var result = product
+console.log(product)
---
+++ sigma.js
@@ -0,0 +1,7 @@
+var sum = 0;
+
+for(var i = 1; i < 101; i++) {
+    sum += i
+}
+var result = sum
+console.log(result)
